b552fa5ba5175661667b707040ccf2f2cfd838f4
Esko Suomi <esko.suomi@solita.fi>
2023-05-19 13:20:37 +0300

    add validation and conversion service stubs

--- src/main/java/fi/digitraffic/tis/vaco/conversion/ConversionService.java
+++ src/main/java/fi/digitraffic/tis/vaco/conversion/ConversionService.java
@@ -1,7 +1,15 @@
 package fi.digitraffic.tis.vaco.conversion;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConversionService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConversionService.class);
+
+    public void convert() {
+        // TODO: conversion process goes here :)
+        LOGGER.info("Convert...");
+    }
 }
