63d73fba4ae4ca00c6b03432127c8daa8971adc9
Esko Suomi <esko.suomi@solita.fi>
2023-05-30 11:12:11 +0300

    replace Jackson2ObjectMapperBuilder with Jackson2ObjectMapperBuilderCustomizer which is the actual intended configuration builder bean type in Spring to fix rendering of timestamps
    This the kind of crap I really dislike in Spring :(

--- src/main/java/fi/digitraffic/tis/vaco/JacksonFeaturesConfiguration.java
+++ src/main/java/fi/digitraffic/tis/vaco/JacksonFeaturesConfiguration.java
@@ -3,9 +3,9 @@ package fi.digitraffic.tis.vaco;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.cfg.CoercionAction;
 import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
  * Customize Spring's internal Jackson facilities.
@@ -17,22 +17,23 @@ public class JacksonFeaturesConfiguration {
      * Configures underlying {@link com.fasterxml.jackson.databind.ObjectMapper} to be as strict as possible without
      * getting in the way.
      *
-     * @return <code>Jackson2ObjectMapperBuilder</code> with strict {@link com.fasterxml.jackson.databind.cfg.CoercionConfig}
+     * @return <code>Jackson2ObjectMapperBuilderCustomizer</code> with strict {@link com.fasterxml.jackson.databind.cfg.CoercionConfig}
      */
     @Bean
-    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
-        return new Jackson2ObjectMapperBuilder()
-                .postConfigurer((objectMapper -> {
-                    // @JsonView made easy
-                    objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
-                    // prevent coercion of anything to strings to allow Jakarta Validation to work as intended
-                    objectMapper.coercionConfigDefaults()
-                            .setCoercion(CoercionInputShape.Boolean, CoercionAction.Fail)
-                            .setCoercion(CoercionInputShape.Integer, CoercionAction.Fail)
-                            .setCoercion(CoercionInputShape.Float, CoercionAction.Fail)
-                            .setCoercion(CoercionInputShape.String, CoercionAction.Fail)
-                            .setCoercion(CoercionInputShape.Array, CoercionAction.Fail)
-                            .setCoercion(CoercionInputShape.Object, CoercionAction.Fail);
-                }));
+    public Jackson2ObjectMapperBuilderCustomizer strictCoercionConfiguration() {
+        return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder.postConfigurer(objectMapper -> {
+                // @JsonView made easy
+                objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+                // prevent coercion of anything to strings to allow Jakarta Validation to work as intended
+                objectMapper.coercionConfigDefaults()
+                        .setCoercion(CoercionInputShape.Boolean, CoercionAction.Fail)
+                        .setCoercion(CoercionInputShape.Integer, CoercionAction.Fail)
+                        .setCoercion(CoercionInputShape.Float, CoercionAction.Fail)
+                        .setCoercion(CoercionInputShape.String, CoercionAction.Fail)
+                        .setCoercion(CoercionInputShape.Array, CoercionAction.Fail)
+                        .setCoercion(CoercionInputShape.Object, CoercionAction.Fail);
+            });
+        };
     }
 }
