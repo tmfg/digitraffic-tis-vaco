6cd17df62973c8cb3e7d2cda296b876f153d2e05
Esko Suomi <esko.suomi@solita.fi>
2023-05-31 16:13:56 +0300

    extract QueueEntry's RowMapper to RowMappers

--- src/main/java/fi/digitraffic/tis/vaco/db/RowMappers.java
+++ src/main/java/fi/digitraffic/tis/vaco/db/RowMappers.java
@@ -1,19 +1,29 @@
 package fi.digitraffic.tis.vaco.db;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.digitraffic.tis.vaco.queuehandler.model.ImmutablePhase;
+import fi.digitraffic.tis.vaco.queuehandler.model.ImmutableQueueEntry;
 import fi.digitraffic.tis.vaco.validation.model.Category;
 import fi.digitraffic.tis.vaco.validation.model.CooperationType;
 import fi.digitraffic.tis.vaco.validation.model.ImmutableCooperation;
 import fi.digitraffic.tis.vaco.validation.model.ImmutableOrganization;
 import fi.digitraffic.tis.vaco.validation.model.ImmutableRuleSet;
+import org.postgresql.util.PGobject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Optional;
 import java.util.function.Function;
 
 public class RowMappers {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RowMappers.class);
+
     public static final RowMapper<ImmutableRuleSet> RULESET = (rs, rowNum) -> ImmutableRuleSet.builder()
             .id(rs.getLong("id"))
             .publicId(rs.getString("public_id"))
@@ -44,8 +54,35 @@ public class RowMappers {
             .partnerA(rs.getLong("partner_a_id"))
             .partnerB(rs.getLong("partner_b_id"))
             .build();
+    public static final Function<ObjectMapper, RowMapper<ImmutableQueueEntry>> QUEUE_ENTRY = RowMappers::mapRow;
 
     private static <I,O> O nullable(I input, Function<I, O> i2o) throws SQLException {
         return Optional.ofNullable(input).map(i2o).orElse(null);
     }
+
+    private static RowMapper<ImmutableQueueEntry> mapRow(ObjectMapper objectMapper) {
+        return (rs, rowNum) -> ImmutableQueueEntry.builder()
+                .id(rs.getLong("id"))
+                .publicId(rs.getString("public_id"))
+                .businessId(rs.getString("business_id"))
+                .format(rs.getString("format"))
+                .url(rs.getString("url"))
+                .etag(rs.getString("etag"))
+                .metadata(readJson(objectMapper, rs, "metadata"))
+                .build();
+    }
+
+    private static JsonNode readJson(ObjectMapper objectMapper, ResultSet rs, String field) {
+        try {
+            PGobject source = (PGobject) rs.getObject(field);
+            if (source != null) {
+                return objectMapper.readTree(source.getValue());
+            }
+        } catch (SQLException | JsonProcessingException e) {
+            LOGGER.error("Failed Jdbc conversion from PGobject to JsonNode", e);
+        }
+        // TODO: This is potentially fatal, we could re-throw instead
+        return null;
+    }
+
 }
