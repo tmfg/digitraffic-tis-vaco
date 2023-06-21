c3804f59d1edda2873aa6ce84e17f70bb22a4870
Esko Suomi <esko.suomi@solita.fi>
2023-06-21 13:14:22 +0300

    fix incorrect ObjectMapper usage by AWSpring's SQS integration by manually defining relevant configuration

--- src/main/java/fi/digitraffic/tis/vaco/aws/AwsConfiguration.java
+++ src/main/java/fi/digitraffic/tis/vaco/aws/AwsConfiguration.java
@@ -1,10 +1,13 @@
 package fi.digitraffic.tis.vaco.aws;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
 import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementOrdering;
 import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
+import io.awspring.cloud.sqs.support.converter.SqsMessagingMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.time.Duration;
@@ -19,7 +22,9 @@ public class AwsConfiguration {
     private static final Duration ONE_SECOND = Duration.ofSeconds(1);
 
     @Bean
-    SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+    SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(
+        SqsAsyncClient sqsAsyncClient,
+        SqsMessagingMessageConverter messageConverter) {
         return SqsMessageListenerContainerFactory
             .builder()
             .configure(options -> options
@@ -32,8 +37,25 @@ public class AwsConfiguration {
                 .listenerShutdownTimeout(ONE_SECOND)
                 .pollTimeout(ONE_SECOND)
                 .acknowledgementShutdownTimeout(Duration.ZERO)
+                .messageConverter(messageConverter)
             )
             .sqsAsyncClient(sqsAsyncClient)
             .build();
+    }
+
+    /**
+     * Getting around a bug in AWSpring (ref. <a href="https://github.com/awspring/spring-cloud-aws/issues/721"> awspring /
+     * spring-cloud-aws #721: Add User's MappingJackson2MessageConverter to SQS Autoconfiguration</a>)
+     *
+     * @param objectMapper Global <code>ObjectMapper</code> instance everything should use.
+     * @return Manually handcrafted, artesanal <code>SqsMessagingMessageConverter</code>
+     */
+    @Bean
+    SqsMessagingMessageConverter sqsMessagingMessageConverter(ObjectMapper objectMapper) {
+        SqsMessagingMessageConverter converter = new SqsMessagingMessageConverter();
+        MappingJackson2MessageConverter actualConverter = new MappingJackson2MessageConverter();
+        actualConverter.setObjectMapper(objectMapper);
+        converter.setPayloadMessageConverter(actualConverter);
+        return converter;
     }
 }
