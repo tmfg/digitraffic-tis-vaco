36af70d17374bbc5f7ff9d4f6f0aa379773ddb60
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2023-08-07 14:05:57 +0300

    Add a CORS rule allowing cross-origin requests on /queue endpoint for local development

--- src/main/java/fi/digitraffic/tis/vaco/VacoApplication.java
+++ src/main/java/fi/digitraffic/tis/vaco/VacoApplication.java
@@ -3,6 +3,9 @@ package fi.digitraffic.tis.vaco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,4 +15,13 @@ public class VacoApplication {
         SpringApplication.run(VacoApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/queue").allowedOrigins("http://localhost:5173");
+            }
+        };
+    }
 }
