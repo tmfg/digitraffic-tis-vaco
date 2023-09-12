587adb07f9f25ad5ec0baf34225f06750ca152f8
Esko Suomi <esko.suomi@solita.fi>
2023-09-12 13:19:45 +0300

    inject a bit more contextual ids to make the magic strings' source more clearly apparent whenever tests fail

--- src/test/java/fi/digitraffic/tis/vaco/TestObjects.java
+++ src/test/java/fi/digitraffic/tis/vaco/TestObjects.java
@@ -37,8 +37,17 @@ public class TestObjects {
 
     public static ImmutableOrganization.Builder anOrganization() {
         return ImmutableOrganization.builder()
-            .businessId(UUID.randomUUID().toString())
-            .name(UUID.randomUUID().toString());
+            .businessId(randomBusinessId())
+            .name("organization:name:" + UUID.randomUUID());
+    }
+
+    private static String randomBusinessId() {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 7; i++) {
+            sb.append(r.nextInt(10));
+        }
+        return sb.append("-").append(r.nextInt(10)).toString();
     }
 
     public static ImmutableCooperation.Builder aCooperation() {
@@ -54,13 +63,13 @@ public class TestObjects {
     public static ImmutableTask.Builder aTask() {
         return ImmutableTask.builder()
             .id(new Random().nextLong())
-            .name(UUID.randomUUID().toString())
+            .name("task:name:" + UUID.randomUUID())
             .priority(new Random().nextInt());
     }
 
     public static ImmutableRuleset.Builder aRuleset() {
         return ImmutableRuleset.builder()
-            .identifyingName(UUID.randomUUID().toString())
+            .identifyingName("rule:identifyingName:" + UUID.randomUUID())
             .description("running hello rule from tests")
             .category(Category.GENERIC)
             .type(Type.VALIDATION_SYNTAX);
