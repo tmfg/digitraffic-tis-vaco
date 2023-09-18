db70708e32084ec4428f956095c588db19677509
Esko Suomi <esko.suomi@solita.fi>
2023-09-18 12:53:25 +0300

    replace MessagingService with contents from SqsMessagingService as it is an unneeded abstraction

--- src/main/java/fi/digitraffic/tis/vaco/messaging/MessagingService.java
+++ src/main/java/fi/digitraffic/tis/vaco/messaging/MessagingService.java
@@ -1,17 +1,113 @@
 package fi.digitraffic.tis.vaco.messaging;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import fi.digitraffic.tis.utilities.model.ProcessingState;
 import fi.digitraffic.tis.vaco.conversion.model.ConversionJobMessage;
 import fi.digitraffic.tis.vaco.messaging.model.DelegationJobMessage;
 import fi.digitraffic.tis.vaco.messaging.model.ImmutableDelegationJobMessage;
-import fi.digitraffic.tis.utilities.model.ProcessingState;
+import fi.digitraffic.tis.vaco.messaging.model.MessageQueue;
+import fi.digitraffic.tis.vaco.queuehandler.repository.QueueHandlerRepository;
+import fi.digitraffic.tis.vaco.rules.model.ValidationRuleJobMessage;
 import fi.digitraffic.tis.vaco.validation.model.ValidationJobMessage;
+import io.awspring.cloud.sqs.operations.MessagingOperationFailedException;
+import io.awspring.cloud.sqs.operations.SendResult;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 
-public interface MessagingService {
-    void submitProcessingJob(DelegationJobMessage delegationJobMessage);
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
-    void submitValidationJob(ValidationJobMessage jobDescription);
+@Service
+public class MessagingService {
 
-    void submitConversionJob(ConversionJobMessage jobDescription);
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    void updateJobProcessingStatus(ImmutableDelegationJobMessage jobDescription, ProcessingState start);
+    private final SqsClient sqsClient;
+    private final SqsTemplate sqsTemplate;
+    private final Cache<String, String> sqsQueueUrlCache;
+    private final QueueHandlerRepository queueHandlerRepository;
+
+    public MessagingService(SqsClient sqsClient,
+                            SqsTemplate sqsTemplate,
+                            Cache<String, String> sqsQueueUrlCache,
+                            QueueHandlerRepository queueHandlerRepository) {
+        this.sqsClient = sqsClient;
+        this.sqsTemplate = sqsTemplate;
+        this.sqsQueueUrlCache = sqsQueueUrlCache;
+        this.queueHandlerRepository = queueHandlerRepository;
+    }
+
+    private <P> CompletableFuture<SendResult<P>> sendMessage(String queueName, P payload) {
+        try {
+            logger.debug("send {} <- {}", queueName, payload);
+            return sqsTemplate.sendAsync(queueName, payload);
+        } catch (MessagingOperationFailedException mofe) {
+            logger.warn("Failed to send message %s to queue %s".formatted(payload, queueName), mofe);
+            return CompletableFuture.failedFuture(mofe);
+        }
+    }
+
+    public CompletableFuture<SendResult<DelegationJobMessage>> submitProcessingJob(DelegationJobMessage delegationJobMessage) {
+        return sendMessage(MessageQueue.JOBS.getQueueName(), delegationJobMessage);
+    }
+
+    public CompletableFuture<SendResult<ValidationJobMessage>> submitValidationJob(ValidationJobMessage jobDescription) {
+        return sendMessage(MessageQueue.JOBS_VALIDATION.getQueueName(), jobDescription);
+    }
+
+   public CompletableFuture<SendResult<ConversionJobMessage>> submitConversionJob(ConversionJobMessage jobDescription) {
+        return sendMessage(MessageQueue.JOBS_CONVERSION.getQueueName(), jobDescription);
+    }
+
+    public void updateJobProcessingStatus(ImmutableDelegationJobMessage jobDescription, ProcessingState state) {
+        switch (state) {
+            case START -> queueHandlerRepository.startEntryProcessing(jobDescription.entry());
+            case UPDATE -> queueHandlerRepository.updateEntryProcessing(jobDescription.entry());
+            case COMPLETE -> queueHandlerRepository.completeEntryProcessing(jobDescription.entry());
+        }
+    }
+
+    public <C> CompletableFuture<SendResult<ValidationRuleJobMessage>> submitRuleExecutionJob(String ruleName, ValidationRuleJobMessage ruleMessage) {
+        return sendMessage(MessageQueue.RULES.munge(ruleName), ruleMessage);
+    }
+
+    public void deleteMessage(String queueName, Message m) {
+        sqsClient.deleteMessage(DeleteMessageRequest.builder()
+            .queueUrl(resolveQueueUrl(queueName))
+            .receiptHandle(m.receiptHandle())
+            .build());
+        logger.debug("delete {} !- {}", queueName, m);
+    }
+
+    public Stream<Message> readMessages(String queueName) {
+        ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+            .queueUrl(resolveQueueUrl(queueName))
+            .maxNumberOfMessages(Runtime.getRuntime().availableProcessors())
+            .waitTimeSeconds(1)
+            .build();
+
+        return sqsClient.receiveMessage(receiveMessageRequest)
+            .messages()
+            .stream()
+            .parallel()
+            .peek(m -> logger.debug("receive {} -> {}", queueName, m));
+    }
+
+    private String resolveQueueUrl(String queueName) {
+        return sqsQueueUrlCache.get(queueName, cachedQueueName -> {
+            String queueUrl = sqsClient.getQueueUrl(GetQueueUrlRequest.builder()
+                .queueName(cachedQueueName)
+                .build()).queueUrl();
+            logger.debug("Resolved URL for queue {} as {}", cachedQueueName, queueUrl);
+            return queueUrl;
+        });
+
+    }
 }
