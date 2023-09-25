e7e9e95a3ec53ae1dcf146ce9f3fbffab390551d
Esko Suomi <esko.suomi@solita.fi>
2023-09-25 15:16:41 +0300

    configure Spring Security to work with Azure AD and CORS properly
    What an adventure in hit-your-face-to-wall this was.

--- src/main/java/fi/digitraffic/tis/vaco/AadOAuth2LoginSecurityConfig.java
+++ src/main/java/fi/digitraffic/tis/vaco/AadOAuth2LoginSecurityConfig.java
@@ -1,5 +1,7 @@
 package fi.digitraffic.tis.vaco;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,24 +9,70 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 @EnableMethodSecurity
+@ConditionalOnProperty(value = "spring.cloud.azure.active-directory.enabled", havingValue = "true")
 public class AadOAuth2LoginSecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-            .cors(Customizer.withDefaults())
-            .authorizeHttpRequests(auth -> auth
-                // If in need to open up some specific endpoint:
-                //.requestMatchers("/path/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           CorsConfigurationSource corsConfigurationSource) throws Exception {
+        return http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            // NOTE: Order is important here! Above is for Azure AD support, below is our extras
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .httpBasic(Customizer.withDefaults())
             .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(@Value("${vaco.uiUrl}") String uiUrl) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(uiUrl));
+        configuration.setAllowedMethods(List.of("OPTIONS", "GET", "POST"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public OAuth2TokenValidator<Jwt> issuerClaimValidator(
+        @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String jwkIssuerUri) {
+        return JwtValidators.createDefaultWithIssuer(jwkIssuerUri);
+    }
+
+    @Bean
+    public OAuth2TokenValidator<Jwt> audienceClaimValidator(@Value("${vaco.azure-ad.client-id}") String clientId) {
+        return new JwtTokenValidator(
+            token -> token.getAudience().contains(clientId),
+            new OAuth2Error(OAuth2ErrorCodes.UNAUTHORIZED_CLIENT));
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String jwkIssuerUri,
+                          List<OAuth2TokenValidator<Jwt>> validators) {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(jwkIssuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(validators);
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
     }
 }
---
+++ src/main/java/fi/digitraffic/tis/vaco/JwtTokenValidator.java
@@ -0,0 +1,28 @@
+package fi.digitraffic.tis.vaco;
+
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.function.Function;
+
+public class JwtTokenValidator implements OAuth2TokenValidator<Jwt> {
+
+    private final Function<Jwt, Boolean> validator;
+    private final OAuth2Error errorOnFailure;
+
+    public JwtTokenValidator(Function<Jwt, Boolean> validator, OAuth2Error errorOnFailure) {
+        this.validator = validator;
+        this.errorOnFailure = errorOnFailure;
+    }
+
+    @Override
+    public OAuth2TokenValidatorResult validate(Jwt token) {
+        if (validator.apply(token)) {
+            return OAuth2TokenValidatorResult.success();
+        } else {
+            return OAuth2TokenValidatorResult.failure(errorOnFailure);
+        }
+    }
+}
