c4fdc253823c8299e675ffa6dbb1b374aff5dc8b
Esko Suomi <esko.suomi@solita.fi>
2023-11-02 15:05:17 +0200

    add license header to credit the creator of the topology sorting utility properly

--- src/main/java/fi/digitraffic/tis/utilities/MoreGraphs.java
+++ src/main/java/fi/digitraffic/tis/utilities/MoreGraphs.java
@@ -35,6 +35,37 @@ import static com.google.common.collect.Multisets.toMultiset;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toCollection;
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021-present Jonathan Bluett-Duncan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/**
+ * Provides Google Guava's Graph compatible topological sorting algorithms.
+ * <p>
+ * Included as source as no readymade packaging exists. See license header above.
+ *
+ * @see <a href="https://github.com/jbduncan/guava-graph-utils">jbduncan/guava-graph-utils</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Topological_sorting">Wikipedia: Topological sorting</a>
+ */
 // This class purposefully expands upon an unstable Guava API
 @SuppressWarnings("UnstableApiUsage")
 public final class MoreGraphs {
