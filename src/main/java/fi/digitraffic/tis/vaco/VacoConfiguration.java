457f9f7eb94aa5b9cf278cc66148911a1d91a003
Esko Suomi <esko.suomi@solita.fi>
2023-11-10 20:10:36 +0200

    configure ample timeouts for AWS clients

--- src/main/java/fi/digitraffic/tis/vaco/VacoConfiguration.java
+++ src/main/java/fi/digitraffic/tis/vaco/VacoConfiguration.java
@@ -6,10 +6,17 @@ import fi.digitraffic.tis.aws.s3.S3Client;
 import fi.digitraffic.tis.http.HttpClient;
 import fi.digitraffic.tis.vaco.configuration.VacoProperties;
 import fi.digitraffic.tis.vaco.ruleset.model.Ruleset;
+import io.awspring.cloud.autoconfigure.core.AwsClientCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
 
 import java.io.IOException;
@@ -35,6 +42,35 @@ public class VacoConfiguration {
                                     software.amazon.awssdk.services.s3.S3Client awsS3Client,
                                     S3TransferManager s3TransferManager) {
         return new S3Client(vacoProperties, s3TransferManager,awsS3Client);
+    }
+
+    @Bean
+    AwsClientCustomizer<S3ClientBuilder> s3ClientBuilderAwsClientConfigurer() {
+        return new S3AwsClientClientConfigurer();
+    }
+
+    static class S3AwsClientClientConfigurer implements AwsClientCustomizer<S3ClientBuilder> {
+        @Override
+        public ClientOverrideConfiguration overrideConfiguration() {
+            return ClientOverrideConfiguration.builder()
+                .apiCallTimeout(Duration.ofSeconds(15))
+                .build();
+        }
+
+        @Override
+        public SdkHttpClient httpClient() {
+            return ApacheHttpClient.builder()
+                .connectionTimeout(Duration.ofSeconds(15))
+                .build();
+        }
+
+        @Override
+        public SdkAsyncHttpClient asyncHttpClient() {
+            return NettyNioAsyncHttpClient.builder()
+                .connectionTimeout(Duration.ofSeconds(15))
+                .writeTimeout(Duration.ofSeconds(15))
+                .build();
+        }
     }
 
     @Bean(name = "rulesetNameCache")
