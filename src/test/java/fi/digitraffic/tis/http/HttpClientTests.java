a288387876a55b5301a14f50b49cacb9c5b21f28
Esko Suomi <esko.suomi@solita.fi>
2024-01-27 13:41:15 +0200

    add true support for gzip and deflate with Methanol library

---
+++ src/test/java/fi/digitraffic/tis/http/HttpClientTests.java
@@ -0,0 +1,35 @@
+package fi.digitraffic.tis.http;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class HttpClientTests {
+
+    private HttpClient httpClient;
+
+    @BeforeEach
+    void setUp() {
+        httpClient = new HttpClient();
+    }
+
+    @Test
+    void canCreateHeadersFromPossiblyNullValues() {
+
+        assertThat(httpClient.headers(), equalTo(Map.of()));
+        assertThat(httpClient.headers("foo", null), equalTo(Map.of()));
+        assertThat(httpClient.headers("foo", "bar"), equalTo(Map.of("foo", "bar")));
+        assertThat(httpClient.headers("foo", "bar", "quu", null), equalTo(Map.of("foo", "bar")));
+        assertThat(httpClient.headers("foo", "bar", "quu", "qux"), equalTo(Map.of("foo", "bar", "quu", "qux")));
+    }
+
+    @Test
+    void throwsExceptionOnUnevenNumberOfHeaderValues() {
+        assertThrows(HttpClientException.class, () -> httpClient.headers("moi"), "Uneven number of values should not be allowed");
+    }
+}
