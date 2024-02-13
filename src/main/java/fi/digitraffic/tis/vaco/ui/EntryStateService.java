55fe006a4c521eea580e60fc4c37c8b19e5f95cb
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2024-02-13 16:00:39 +0200

    Improve performance of ui findings report generation

--- src/main/java/fi/digitraffic/tis/vaco/ui/EntryStateService.java
+++ src/main/java/fi/digitraffic/tis/vaco/ui/EntryStateService.java
@@ -5,6 +5,7 @@ import fi.digitraffic.tis.utilities.dto.Link;
 import fi.digitraffic.tis.utilities.dto.Resource;
 import fi.digitraffic.tis.vaco.configuration.VacoProperties;
 import fi.digitraffic.tis.vaco.findings.Finding;
+import fi.digitraffic.tis.vaco.findings.FindingRepository;
 import fi.digitraffic.tis.vaco.packages.model.Package;
 import fi.digitraffic.tis.vaco.process.model.Task;
 import fi.digitraffic.tis.vaco.queuehandler.model.Entry;
@@ -15,6 +16,7 @@ import fi.digitraffic.tis.vaco.summary.model.gtfs.Agency;
 import fi.digitraffic.tis.vaco.summary.model.gtfs.FeedInfo;
 import fi.digitraffic.tis.vaco.ui.model.AggregatedFinding;
 import fi.digitraffic.tis.vaco.ui.model.ImmutableAggregatedFinding;
+import fi.digitraffic.tis.vaco.ui.model.ImmutableItemCounter;
 import fi.digitraffic.tis.vaco.ui.model.ImmutableRuleReport;
 import fi.digitraffic.tis.vaco.ui.model.ItemCounter;
 import fi.digitraffic.tis.vaco.ui.model.RuleReport;
@@ -27,10 +29,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.fromMethodCall;
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
@@ -39,26 +43,48 @@ import static org.springframework.web.servlet.mvc.method.annotation.MvcUriCompon
 public class EntryStateService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    private final EntryStateRepository entryStateRepository;
+    private final FindingRepository findingRepository;
     private final SummaryRepository summaryRepository;
     private final VacoProperties vacoProperties;
 
-    public EntryStateService(EntryStateRepository entryStateRepository, SummaryRepository summaryRepository, VacoProperties vacoProperties) {
-        this.entryStateRepository = Objects.requireNonNull(entryStateRepository);
+    public EntryStateService(FindingRepository findingRepository, SummaryRepository summaryRepository, VacoProperties vacoProperties) {
+        this.findingRepository = Objects.requireNonNull(findingRepository);
         this.summaryRepository = Objects.requireNonNull(summaryRepository);
         this.vacoProperties = Objects.requireNonNull(vacoProperties);
     }
 
     public RuleReport getRuleReport(Task task, Entry entry, Map<String, Ruleset> rulesets) {
-        List<ItemCounter> counters = entryStateRepository.findFindingCounters(task.id());
+        List<Finding> allFindings = findingRepository.findFindingsByTaskId(task.id());
 
-        List<AggregatedFinding> aggregated = entryStateRepository.findAggregatedFindings(task.id());
+        Map<String, Long> findingCountersBySeverity =
+            allFindings.stream().collect(Collectors.groupingBy(f -> f.severity().toUpperCase(),
+                Collectors.counting()));
+        // Order of severities matters for UI:
+        List<String> countersBySeverityKeys = new ArrayList<>(findingCountersBySeverity.keySet());
+        countersBySeverityKeys.sort(new SortStringsBySeverity());
+        List<ItemCounter> counters = new ArrayList<>();
+        counters.add(ImmutableItemCounter.builder()
+            .name("ALL")
+            .total(allFindings.size())
+            .build());
+        countersBySeverityKeys.forEach(severity ->
+            counters.add(ImmutableItemCounter.builder()
+                .name(severity)
+                .total(findingCountersBySeverity.get(severity))
+                .build())
+        );
+
+        Map<String, List<Finding>> findingsByMessage =
+            allFindings.stream().collect(Collectors.groupingBy(f -> f.message().toLowerCase()));
         List<AggregatedFinding> aggregatedWithFindings = new ArrayList<>();
-        aggregated.forEach(aggregatedFinding -> {
-            List<Finding> findings = entryStateRepository.findNoticeFindings(task.id(), aggregatedFinding.code());
-            AggregatedFinding aggregatedWithFindingInstances = ImmutableAggregatedFinding.copyOf(aggregatedFinding).withFindings(findings);
-            aggregatedWithFindings.add(aggregatedWithFindingInstances);
-        });
+        findingsByMessage.forEach((code, findings) -> aggregatedWithFindings.add(ImmutableAggregatedFinding.builder()
+                .code(code)
+                .severity(findings.get(0).severity())
+                .total(findings.size())
+                .findings(findings)
+            .build()));
+        // aggregatedWithFindings should also be ordered from highest to lowest severity:
+        aggregatedWithFindings.sort(new SortFindingsBySeverity());
 
         List<Package> taskPackages = Streams.filter(entry.packages(), p -> p.taskId().equals(task.id())).toList();
         Ruleset rule = rulesets.get(task.name());
@@ -133,5 +159,31 @@ public class EntryStateService {
             .value(feedInfo.getFeedEndDate())
             .build());
         return feedInfoContent;
+    }
+
+    private static int severityToInt(String severity) {
+        return switch (severity) {
+            case "CRITICAL" -> 1;
+            case "ERROR" -> 2;
+            case "WARNING" -> 3;
+            case "INFO" -> 4;
+            default -> 5;
+        };
+    }
+
+    static class SortStringsBySeverity implements Comparator<String> {
+        public int compare(String s1, String s2) {
+            int severity1 = severityToInt(s1);
+            int severity2 = severityToInt(s2);
+            return severity1 - severity2;
+        }
+    }
+
+    static class SortFindingsBySeverity implements Comparator<AggregatedFinding> {
+        public int compare(AggregatedFinding f1, AggregatedFinding f2) {
+            int severity1 = severityToInt(f1.severity());
+            int severity2 = severityToInt(f2.severity());
+            return severity1 - severity2;
+        }
     }
 }
