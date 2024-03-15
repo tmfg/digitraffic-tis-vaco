ee5f1ab469f31a25bb8c0a5ee8fc719f186482d5
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2024-03-15 10:58:08 +0200

    Data delivery CSV export

--- src/main/java/fi/digitraffic/tis/vaco/ui/AdminToolsService.java
+++ src/main/java/fi/digitraffic/tis/vaco/ui/AdminToolsService.java
@@ -1,5 +1,6 @@
 package fi.digitraffic.tis.vaco.ui;
 
+import com.opencsv.CSVWriter;
 import fi.digitraffic.tis.utilities.Streams;
 import fi.digitraffic.tis.vaco.company.model.Company;
 import fi.digitraffic.tis.vaco.me.MeService;
@@ -8,6 +9,7 @@ import fi.digitraffic.tis.vaco.ui.model.CompanyWithFormatSummary;
 import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -28,6 +30,57 @@ public class AdminToolsService {
             ? userCompanies.stream().map(Company::businessId).collect(Collectors.toSet())
             : null;
         return adminToolsRepository.listCompanyLatestEntries(businessIds);
+    }
+
+    public String[] getDataDeliveryCsvHeaders(String language) {
+        switch (language) {
+            case "en" -> {
+                return new String[]{"Business ID", "Company name", "Data URL",
+                    "Feed name", "Format", "Converted format(-s)", "Status", "Date published", "Entry public ID"};
+            }
+            case "sv" -> {
+                return new String[]{"Företags-id", "Företagsnamn", "Data-URL",
+                    "Matningsnamn", "Format", "Konverterad", "Status", "Skapad", "Inlämnings-ID"};
+            }
+            default -> {
+                return new String[]{"Y-tunnus", "Yrityksen nimi", "Datan URL",
+                    "Syötteen nimi", "Formaatti", "Konversio", "Tila", "Julkaistu", "Julkaisun tunnus"};
+            }
+        }
+    }
+
+    public List<CompanyLatestEntry> getDataDeliveryOverview(@Nullable Set<Company> userCompanies) {
+        Set<String> businessIds = userCompanies != null
+            ? userCompanies.stream().map(Company::businessId).collect(Collectors.toSet())
+            : null;
+        return adminToolsRepository.getDataDeliveryOverview(businessIds);
+    }
+
+    public void exportDataDeliveryToCsv(CSVWriter csvWriter, String language) {
+        csvWriter.writeNext(getDataDeliveryCsvHeaders(language));
+
+        List<CompanyLatestEntry> data = adminToolsRepository.getDataDeliveryOverview(null);
+        String currentBusinessId = "";
+        for (CompanyLatestEntry entry : data) {
+            boolean putEmptyString = false;
+            if (currentBusinessId.equals(entry.businessId())) {
+                putEmptyString = true;
+            } else {
+                currentBusinessId = entry.businessId();
+            }
+
+            csvWriter.writeNext(new String[]{
+                putEmptyString ? "" : entry.businessId(),
+                putEmptyString ? "" : entry.companyName(),
+                entry.url(),
+                entry.feedName(),
+                entry.format(),
+                entry.convertedFormat(),
+                entry.status() != null ? entry.status().fieldName() : "",
+                entry.created() != null ? entry.created().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")) : "",
+                entry.publicId()
+            });
+        }
     }
 
     public List<CompanyWithFormatSummary> getCompaniesWithFormatInfos() {
