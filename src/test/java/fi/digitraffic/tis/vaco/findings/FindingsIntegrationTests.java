faef1e7ea0628665a49633c76226868ace8f3d7e
Areta Santos <areta.santos@solita.fi>
2024-05-20 14:16:48 +0300

    class names fixed, method call updated

--- src/test/java/fi/digitraffic/tis/vaco/findings/FindingsIntegrationTests.java
+++ src/test/java/fi/digitraffic/tis/vaco/findings/FindingsIntegrationTests.java
@@ -4,14 +4,14 @@ import fi.digitraffic.tis.SpringBootIntegrationTestBase;
 import fi.digitraffic.tis.vaco.TestObjects;
 import fi.digitraffic.tis.vaco.db.mapper.RecordMapper;
 import fi.digitraffic.tis.vaco.db.model.ContextRecord;
+import fi.digitraffic.tis.vaco.db.model.EntryRecord;
 import fi.digitraffic.tis.vaco.entries.EntryRepository;
 import fi.digitraffic.tis.vaco.findings.model.Finding;
 import fi.digitraffic.tis.vaco.findings.model.FindingSeverity;
-import fi.digitraffic.tis.vaco.process.TaskRepository;
+import fi.digitraffic.tis.vaco.db.repositories.TaskRepository;
 import fi.digitraffic.tis.vaco.process.model.ImmutableTask;
 import fi.digitraffic.tis.vaco.process.model.Task;
 import fi.digitraffic.tis.vaco.queuehandler.model.ImmutableEntry;
-import fi.digitraffic.tis.vaco.queuehandler.model.PersistentEntry;
 import fi.digitraffic.tis.vaco.rules.RuleName;
 import fi.digitraffic.tis.vaco.ruleset.RulesetRepository;
 import fi.digitraffic.tis.vaco.ruleset.model.Ruleset;
@@ -44,7 +44,7 @@ public class FindingsIntegrationTests extends SpringBootIntegrationTestBase {
     RecordMapper recordMapper;
 
     private Task task;
-    private PersistentEntry entry;
+    private EntryRecord entry;
     private Ruleset rule;
 
     @BeforeEach
@@ -52,7 +52,7 @@ public class FindingsIntegrationTests extends SpringBootIntegrationTestBase {
 
         ImmutableEntry entryToCreate = TestObjects.anEntry("gtfs").build();
         entry = entryRepository.create(Optional.empty(), entryToCreate).get();
-        taskRepository.createTasks(List.of(ImmutableTask.of(entry.id(), RuleName.GTFS_CANONICAL, 1)));
+        taskRepository.createTasks(entry, List.of(ImmutableTask.of(RuleName.GTFS_CANONICAL,1)));
         task = taskRepository.findTask(entry.id(), RuleName.GTFS_CANONICAL).get();
         rule = rulesetRepository.findByName(RuleName.GTFS_CANONICAL).get();
 
