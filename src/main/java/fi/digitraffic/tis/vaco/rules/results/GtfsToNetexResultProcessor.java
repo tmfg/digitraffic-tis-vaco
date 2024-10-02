a8a997d58ed61f78ef6778ea436d61904c3bc9b0
Areta Santos <areta.santos@solita.fi>
2024-10-02 10:44:04 +0300

    Summary generated from _stats.json file in gtfs2netex

--- src/main/java/fi/digitraffic/tis/vaco/rules/results/GtfsToNetexResultProcessor.java
+++ src/main/java/fi/digitraffic/tis/vaco/rules/results/GtfsToNetexResultProcessor.java
@@ -15,13 +15,18 @@ import fi.digitraffic.tis.vaco.process.model.Task;
 import fi.digitraffic.tis.vaco.queuehandler.model.Entry;
 import fi.digitraffic.tis.vaco.rules.model.ResultMessage;
 import fi.digitraffic.tis.vaco.ruleset.RulesetService;
+import fi.digitraffic.tis.vaco.summary.GtfsInputSummaryService;
+import fi.digitraffic.tis.vaco.summary.model.RendererType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -32,17 +37,19 @@ public class GtfsToNetexResultProcessor extends RuleResultProcessor implements R
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final RulesetService rulesetService;
     private final ObjectMapper objectMapper;
+    private final GtfsInputSummaryService gtfsInputSummaryService;
 
-    protected GtfsToNetexResultProcessor(VacoProperties vacoProperties,
-                                         PackagesService packagesService,
-                                         S3Client s3Client,
-                                         TaskService taskService,
-                                         RulesetService rulesetService,
-                                         FindingService findingService,
-                                         ObjectMapper objectMapper) {
+    public GtfsToNetexResultProcessor(VacoProperties vacoProperties,
+                                      PackagesService packagesService,
+                                      S3Client s3Client,
+                                      TaskService taskService,
+                                      RulesetService rulesetService,
+                                      FindingService findingService,
+                                      ObjectMapper objectMapper, GtfsInputSummaryService gtfsInputSummaryService) {
         super(vacoProperties, packagesService, s3Client, taskService, findingService);
         this.rulesetService = Objects.requireNonNull(rulesetService);
         this.objectMapper = Objects.requireNonNull(objectMapper);
+        this.gtfsInputSummaryService = Objects.requireNonNull(gtfsInputSummaryService);
     }
 
     @Override
@@ -52,6 +59,21 @@ public class GtfsToNetexResultProcessor extends RuleResultProcessor implements R
 
         Map<String, String> fileNames = collectOutputFileNames(resultMessage);
 
+        String stats = "_stats.json";
+
+        boolean statsProcessed = fileNames.keySet().stream()
+            .filter(key -> key.endsWith(stats))
+            .findFirst()
+            .map(key -> processFile(resultMessage, entry, task, fileNames, key, path -> {
+                try {
+                    ArrayList<String> gtfs2netexStats = scanStatsFileToArray(path);
+                    return saveGtfs2NetexSummary(task, gtfs2netexStats);
+                } catch (JSONException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }))
+            .orElse(false);
+
         boolean errorsProcessed = processFile(resultMessage, entry, task, fileNames, "errors.json", path -> {
             List<Finding> findings = new ArrayList<>(scanErrorFile(entry, task, resultMessage.ruleName(), path));
             return storeFindings(findings);
@@ -59,7 +81,7 @@ public class GtfsToNetexResultProcessor extends RuleResultProcessor implements R
 
         resolveTaskStatus(entry, task);
 
-        return errorsProcessed;
+        return errorsProcessed && statsProcessed;
     }
 
     private List<ImmutableFinding> scanErrorFile(Entry entry, Task task, String ruleName, Path reportsFile) {
@@ -75,26 +97,48 @@ public class GtfsToNetexResultProcessor extends RuleResultProcessor implements R
                     if (errorMsg == null) {
                         return null;
                     }
-                        try {
-                            return ImmutableFinding.of(
-                                    task.id(),
-                                    rulesetId,
-                                    ruleName,
-                                    errorMsg,
-                                    "ERROR"
-                                    )
-                                .withRaw(objectMapper.writeValueAsBytes(errorMap));
-                        } catch (JsonProcessingException e) {
-                            logger.warn("Failed to convert tree to bytes", e);
-                            return null;
-                        }
-                    })
+                    try {
+                        return ImmutableFinding.of(
+                                task.id(),
+                                rulesetId,
+                                ruleName,
+                                errorMsg,
+                                "ERROR"
+                            )
+                            .withRaw(objectMapper.writeValueAsBytes(errorMap));
+                    } catch (JsonProcessingException e) {
+                        logger.warn("Failed to convert tree to bytes", e);
+                        return null;
+                    }
+                })
                 .filter(Objects::nonNull)
                 .toList();
         } catch (IOException e) {
             logger.warn("Failed to process {}/{}/{} output file", entry.publicId(), task.name(), ruleName, e);
             return List.of();
         }
+    }
+
+    private ArrayList<String> scanStatsFileToArray(Path reportsFile) throws JSONException, IOException {
+        Map<String, Integer> statsListFile = objectMapper.readValue(reportsFile.toFile(), new TypeReference<>() {});
+
+        JSONObject jsonObject = new JSONObject(statsListFile);
+
+        ArrayList<String> statsList = new ArrayList<>();
+
+        Iterator<String> keys = jsonObject.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            String value = key + ": " + jsonObject.get(key);
+            statsList.add(value);
+        }
+
+        return statsList;
+    }
+
+    private boolean saveGtfs2NetexSummary(Task task, ArrayList<String> gtfs2netexStats) {
+        gtfsInputSummaryService.persistTaskSummaryItem(task, "counts", RendererType.LIST, gtfs2netexStats);
+        return true;
     }
 }
 
