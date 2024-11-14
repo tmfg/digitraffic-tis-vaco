9a60068f6bc275dd77cd4b43e5fb48e81d4dadc3
Areta Santos <areta.santos@solita.fi>
2024-11-14 12:20:38 +0200

    Add CreateFeedRequest

---
+++ src/main/java/fi/digitraffic/tis/vaco/api/model/feed/CreateFeedRequest.java
@@ -0,0 +1,25 @@
+package fi.digitraffic.tis.vaco.api.model.feed;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import fi.digitraffic.tis.vaco.company.model.Company;
+import fi.digitraffic.tis.vaco.db.model.CompanyRecord;
+import fi.digitraffic.tis.vaco.feeds.model.FeedUri;
+import fi.digitraffic.tis.vaco.ruleset.model.TransitDataFormat;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableCreateFeedRequest.class)
+@JsonDeserialize(as = ImmutableCreateFeedRequest.class)
+public interface CreateFeedRequest {
+
+    @Value.Parameter
+    String owner();
+    @Value.Parameter
+    FeedUri uri();
+    @Value.Parameter
+    TransitDataFormat format();
+    @Value.Parameter
+    boolean processingEnabled();
+
+}
