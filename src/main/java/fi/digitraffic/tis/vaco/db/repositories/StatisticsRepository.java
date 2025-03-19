93d5b088de89c263a15aba692b173dfceda0da75
Areta Santos <areta.santos@solita.fi>
2025-03-19 10:54:31 +0200

    Create service and repository for handling stats

---
+++ src/main/java/fi/digitraffic/tis/vaco/db/repositories/StatisticsRepository.java
@@ -0,0 +1,42 @@
+package fi.digitraffic.tis.vaco.db.repositories;
+
+import fi.digitraffic.tis.vaco.db.RowMappers;
+import fi.digitraffic.tis.vaco.db.model.StatusStatisticsRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StatisticsRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final JdbcTemplate jdbc;
+
+    public StatisticsRepository(JdbcTemplate jdbcTemplate, JdbcTemplate jdbc) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.jdbc = jdbc;
+    }
+
+    public List<StatusStatisticsRecord> listStatusStatistics () {
+        try {
+            return jdbc.query("""
+                SELECT *
+                  FROM status_statistics
+                """,
+                RowMappers.STATUS_STATISTICS_RECORD());
+        } catch (EmptyResultDataAccessException e) {
+            return List.of();
+        }
+    }
+
+    public void refreshView() {
+        String refreshSql = "REFRESH MATERIALIZED VIEW status_statistics";
+        jdbcTemplate.execute(refreshSql);
+    }
+}
