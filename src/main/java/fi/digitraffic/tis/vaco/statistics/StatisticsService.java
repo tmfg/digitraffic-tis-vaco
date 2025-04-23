dcbb4687aa21abcd960402e1f38e93df3dead7d9
Areta Santos <areta.santos@solita.fi>
2025-04-23 09:33:32 +0300

    Add scheduled update to  materialized views and add fetch for task statistics

--- src/main/java/fi/digitraffic/tis/vaco/statistics/StatisticsService.java
+++ src/main/java/fi/digitraffic/tis/vaco/statistics/StatisticsService.java
@@ -1,29 +1,44 @@
 package fi.digitraffic.tis.vaco.statistics;
 
 import fi.digitraffic.tis.vaco.db.mapper.RecordMapper;
-import fi.digitraffic.tis.vaco.db.model.StatusStatisticsRecord;
 import fi.digitraffic.tis.vaco.db.repositories.StatisticsRepository;
-import fi.digitraffic.tis.vaco.statistics.model.StatusStatistics;
+import fi.digitraffic.tis.vaco.statistics.model.Statistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class StatisticsService {
-
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final StatisticsRepository statisticsRepository;
     private final RecordMapper recordMapper;
+
     private StatisticsService(StatisticsRepository statisticsRepository, RecordMapper recordMapper) {
         this.statisticsRepository = statisticsRepository;
         this.recordMapper = recordMapper;
     }
 
-    public void refreshMaterializedView() {
-        statisticsRepository.refreshView();
+    @Scheduled(cron = "${vaco.scheduling.daily-statistics-refresh.cron}")
+    public void dailyStatistics() {
+        try {
+            statisticsRepository.refreshView();
+        } catch (Exception e) {
+            logger.warn("Failed to refresh status views ", e);
+        }
+
     }
 
-    public List<StatusStatistics> fetchAllStatistics() {
-        return statisticsRepository.listStatusStatistics().stream()
+    public List<Statistics> fetchAllEntryStatistics() {
+        return statisticsRepository.listEntryStatusStatistics().stream()
+            .map(recordMapper::toStatusStatistics).toList();
+
+    }
+
+    public List<Statistics> fetchAllTaskStatistics() {
+        return statisticsRepository.listTaskStatusStatistics().stream()
             .map(recordMapper::toStatusStatistics).toList();
 
     }
