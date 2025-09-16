a21fd9e7a3d0a72fa5a30ee91806548bc0e19429
Areta Santos <areta.santos@solita.fi>
2025-09-16 08:46:38 +0300

    Add needed imports and refactor

--- src/main/java/fi/digitraffic/tis/vaco/rules/results/GbfsEnturResultProcessor.java
+++ src/main/java/fi/digitraffic/tis/vaco/rules/results/GbfsEnturResultProcessor.java
@@ -27,7 +27,6 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
