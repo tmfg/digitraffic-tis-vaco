60c30839abd4da7f449880300b9cb175ffa1024d
ShamshadAkther <shamshad.akther@solita.fi>
2025-11-19 10:40:50 +0200

    remove unnecessary import

--- src/main/java/fi/digitraffic/tis/vaco/packages/PackagesService.java
+++ src/main/java/fi/digitraffic/tis/vaco/packages/PackagesService.java
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
